dd69bddd53771d2b48b6f598e3932362ee350058
Savannah C <sgcorrell@gmail.com>
2019-03-09 22:40:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,10 @@ def key_for_min_value(name_hash)
  lowest_value = nil
  
  name_hash collect.do |key, value|
-   
-   
-   key
- end
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
 end
